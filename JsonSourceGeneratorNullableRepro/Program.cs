7edd8eece7c8cc7df2356c1c7a0fe400e413ecef
AgmasGold <48861520+AgmasGold@users.noreply.github.com>
2021-09-22 08:15:06 +0100

    Initial commit

---
+++ JsonSourceGeneratorNullableRepro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+Console.WriteLine("JSON Source Generator repro");
+
+public class Class
+{
+    public Uri? NullableUri { get; set; }
+    public byte[]? ByteArray { get; set; }
+}
+
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+[JsonSerializable(typeof(Class))]
+internal partial class Context : JsonSerializerContext
+{
+
+}
